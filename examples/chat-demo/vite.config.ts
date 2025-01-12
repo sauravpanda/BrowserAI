2d502b5d2aeeb6d45eca8337bf0e0c14da6dfd7a
Saurav Panda <sgp65@cornell.edu>
2025-01-12 07:50:15 -0800

    console issue fixes

--- examples/chat-demo/vite.config.ts
+++ examples/chat-demo/vite.config.ts
@@ -3,14 +3,23 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sauravpanda/BrowserAI/',
+  base: 'sauravpanda/BrowserAI',
   plugins: [react()],
   build: {
     commonjsOptions: {
       include: [/@browserai\/browserai/, /node_modules/]
-    }
+    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
   },
   optimizeDeps: {
     include: ['@browserai/browserai']
   },
+  server: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/javascript'
+    }
+  }
 })
