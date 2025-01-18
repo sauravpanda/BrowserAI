edaf1467d0124668751bd462adf7b39590753952
Saurav Panda <sgp65@cornell.edu>
2025-01-18 12:21:23 -0800

    build issue fixes

--- examples/audio-demo/src/components/ChatInterface.tsx
+++ examples/audio-demo/src/components/ChatInterface.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import { BrowserAI } from '@browserai/browserai';
 import posthog from 'posthog-js';
