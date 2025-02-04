1fa4b37da078e2f54684ebdd5663fa45ce0a58a1
johannaschneidr <126594430+johannaschneidr@users.noreply.github.com>
2025-02-03 22:34:22 -0500

    updated styling of workflow-list and chat components to match web app

--- extensions/chrome/src/popup/popup.tsx
+++ extensions/chrome/src/popup/popup.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react"
 import { WorkflowList } from "./workflow-list"
-import { Header } from "./header"
+{/*import { Header } from "./header"*/}
 import { Navigation } from "./navigation"
 import { createRoot } from 'react-dom/client'
 import { ChatInterface } from "./chat-interface"
@@ -59,12 +59,10 @@ export default function Popup() {
 
   return (
     <div className="w-[400px] h-screen bg-background text-foreground flex flex-col">
-      <Header />
-
+      {/* <Header /> */}
       <main className="flex-1 overflow-y-auto bg-background">
         {renderView()}
       </main>
-
       <Navigation />
     </div>
   )
